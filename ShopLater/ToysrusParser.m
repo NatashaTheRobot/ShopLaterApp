a834294756498dfb16245d752a8daaf2a4d5feb3
aug2uag <aug2uag@gmail.com>
2013-06-12 08:03:59 -0700

    switched to native parser

--- ShopLater/ToysrusParser.m
+++ ShopLater/ToysrusParser.m
@@ -7,14 +7,15 @@
 //
 
 #import "ToysrusParser.h"
-#import "TFHpple.h"
 #import "Price.h"
 #import "Image+SLExtensions.h"
 #import "CoreDataManager.h"
 
 @interface ToysrusParser ()
+{
+    NSString *htmlString;
+}
 
-@property (strong, nonatomic) TFHpple *hpple;
 @property (strong, nonatomic) CoreDataManager *coreDataManager;
 
 @property (strong, nonatomic) NSString *name;
@@ -42,8 +43,8 @@
     
     NSURL *url = [NSURL URLWithString:[NSString stringWithFormat:@"http://www.toysrus.com/product/index.jsp?productId=%@", productId]];
     NSData *data = [NSData dataWithContentsOfURL:url];
-    
-    parser.hpple = [TFHpple hppleWithHTMLData:data];
+    parser->htmlString = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
+
     parser.coreDataManager = [CoreDataManager sharedManager];
     
     return parser;
@@ -66,42 +67,36 @@
 
 # pragma mark - Property Delegate Methods
 
-- (NSString *)productName
-{
-    if (!self.name) {
-        
-        NSString *namesPath = @"//div[@id='wrapper']/div[@id='container']/div[@id='productPanel']/div[@id='rightSide']/div[@id='priceReviewAge']/h1";
-        NSArray *namesArray = [self.hpple searchWithXPathQuery:namesPath];
-        self.name = [[namesArray[0] firstChild] content];
-    }
-    
-    return self.name;
-}
-
-- (NSString *)productSummary
-{
-    if (!self.summary) {
-        NSString *summariesPath = @"//div[@id='wrapper']/div[@id='container']/div[@id='infoPanel']/dl[@id='tabset_productPage']/dd/p";
-        NSArray *summariesArray = [self.hpple searchWithXPathQuery:summariesPath];
-        NSString *summary = [summariesArray[0] valueForKeyPath:@"raw"];
-        NSRange rangeString;
-        while ((rangeString = [summary rangeOfString:@"<[^>]+>" options:NSRegularExpressionSearch]).location != NSNotFound)
-            summary = [summary stringByReplacingCharactersInRange:rangeString withString:@" "];
-        
-        summary = [summary stringByReplacingOccurrencesOfString:@"Product Description" withString:@""];
-        self.summary = [summary stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]];
-    }
-    return self.summary;
-}
-
 - (Price *)productPrice
 {
+   
     if (!self.price) {
         
-        NSString *pricesPath = @"//div[@id='wrapper']/div[@id='container']/div[@id='productPanel']/div[@id='rightSide']/div[@id='buyFind']/div[@id='buyWrapper']/div[@id='buyInterior']/div[@id='price']/ul/li[@class='retail']/span";
-        NSArray *pricesArray = [self.hpple searchWithXPathQuery:pricesPath];
-        NSString *priceText = [[pricesArray[0] firstChild] content];
-        NSNumber *priceInDollars = [NSNumber numberWithFloat:[[priceText substringFromIndex:1] floatValue]];
+        NSScanner *scanner = [[NSScanner alloc] initWithString:htmlString];
+        
+        scanner.scanLocation = 0;
+        
+        NSString *startTag = @"<li class=\"retail\">";
+        NSString *endTag = @"</li>";
+        
+        NSString *priceString = nil;
+        
+        
+        [scanner scanUpToString:startTag intoString:nil];
+        scanner.scanLocation += [startTag length];
+        [scanner scanUpToString:endTag intoString:&priceString];
+        
+        
+        scanner = [[NSScanner alloc] initWithString:priceString];
+        startTag = @"&#036;";
+        endTag = @"</span>";
+        
+        [scanner scanUpToString:startTag intoString:nil];
+        scanner.scanLocation += [startTag length];
+        [scanner scanUpToString:endTag intoString:&priceString];
+  
+        
+        NSNumber *priceInDollars = [NSNumber numberWithFloat:[priceString floatValue]];
         
         NSDictionary *priceDictionary = [NSDictionary dictionaryWithObjectsAndKeys:
                                          priceInDollars, @"dollarAmount",
@@ -115,18 +110,115 @@
     return self.price;
 }
 
+
+- (NSString *)productName
+{
+    
+    if (!self.name) {
+        
+        NSScanner *scanner = [[NSScanner alloc] initWithString:htmlString];
+                
+        NSString *startTag = @"<div id=\"priceReviewAge\">";
+        NSString *endTag = @"<h3>";
+        
+        NSString *nameString = nil;
+        
+        
+        [scanner scanUpToString:startTag intoString:nil];
+        scanner.scanLocation += [startTag length];
+        [scanner scanUpToString:endTag intoString:&nameString
+         ];
+        
+        
+        scanner = [[NSScanner alloc] initWithString:nameString];
+        
+        startTag = @"<h1>";
+        endTag = @"</h1>";
+        
+        [scanner scanUpToString:startTag intoString:&nameString];
+        scanner.scanLocation += [startTag length];
+        [scanner scanUpToString:endTag intoString:&nameString];
+        
+        self.name = nameString;
+    }
+    
+    return self.name;
+}
+
+- (NSString *)productSummary
+{
+    if (!self.summary) {
+        
+        NSScanner *scanner = [[NSScanner alloc] initWithString:htmlString];
+        
+        NSString *startTag = @"<label>Product Description</label>";
+        NSString *endTag = @"<p>";
+        
+        NSString *descriptionString = nil;
+        
+        
+        [scanner scanUpToString:startTag intoString:nil];
+        scanner.scanLocation += [startTag length];
+        [scanner scanUpToString:endTag intoString:&descriptionString
+         ];
+        
+        
+        startTag = @"<br />";
+        endTag = @"<br />";
+        
+        scanner = [[NSScanner alloc] initWithString:descriptionString];
+        [scanner scanUpToString:startTag intoString:nil];
+        scanner.scanLocation += [startTag length];
+        [scanner scanUpToString:endTag intoString:&descriptionString
+         ];
+        
+        self.summary = descriptionString;
+        
+        
+    }
+    return self.summary;
+}
+
+
+
 - (Image *)productImage
 {
     if (!self.image) {
+                
+        NSScanner *scanner = [[NSScanner alloc] initWithString:htmlString];
         
-        NSString *imagesPath = @"//div[@id='wrapper']/div[@id='container']/div[@id='productPanel']/div[@id='leftSide']/div[@id='productView']";
-        NSArray *imagesArray = [self.hpple searchWithXPathQuery:imagesPath];
-        NSString *imageURLText = [[[imagesArray[0] childrenArray] valueForKeyPath:@"nodeChildArray.nodeChildArray.nodeChildArray.nodeAttributeArray"][1][1][1][0][0] valueForKey:@"nodeContent"];
+        NSString *startTag = @"dtmTag.dtmc_prod_img =";
+        NSString *endTag = @";";
         
-        NSString *imageFileName = [Image imageFileNameForURL:[NSURL URLWithString:imageURLText]];
+        NSString *imageString = nil;
+
+        
+        [scanner scanUpToString:startTag intoString:nil];
+        scanner.scanLocation += [startTag length];
+        [scanner scanUpToString:endTag intoString:&imageString
+         ];
+        
+        scanner = [[NSScanner alloc] initWithString:imageString];
+        startTag = @"\"";
+        endTag = @"\"";
+        
+        [scanner scanUpToString:startTag intoString:nil];
+        scanner.scanLocation += [startTag length];
+        [scanner scanUpToString:endTag intoString:&imageString
+         ];
+        
+        NSLog(@"%@", imageString);
+        
+        NSString *urlString = [NSString stringWithFormat:@"http://toysrus.com%@", imageString];
+        
+        NSURL *urlImage = [NSURL URLWithString:urlString];
+        
+    
+        
+        NSString *imageFileName = [Image imageFileNameForURL:urlImage];
         
         NSDictionary *imageDictionary = [NSDictionary dictionaryWithObjectsAndKeys:imageFileName, @"fileName",
-                               [NSString stringWithFormat:@"http://toysrus.com%@", imageURLText], @"externalURLString",
+                               urlString, @"externalURLString",
                                                                                     nil];
         self.image = [self.coreDataManager createEntityWithClassName:NSStringFromClass([Image class]) atributesDictionary:imageDictionary];
         

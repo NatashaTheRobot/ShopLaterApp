825aa86115fe31fab7895d0a24bf449295afa10c
Natasha Murashev <nmurashev@gmail.com>
2013-06-14 07:55:45 -0500

    made the table view single selection

--- ProductsListViewController.m
+++ ProductsListViewController.m
@@ -156,9 +156,4 @@
     return cell;
 }
 
-- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    [self.tableView deselectRowAtIndexPath:indexPath animated:NO];
-}
-
 @end

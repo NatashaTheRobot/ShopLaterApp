a3f144875658627cc8d88cd063d72963b5526214
Natasha Murashev <nmurashev@gmail.com>
2013-06-17 18:14:36 -0500

    fixed queues

--- ShopLater/NewProductViewController.m
+++ ShopLater/NewProductViewController.m
@@ -45,7 +45,7 @@
 
     [self.activityIndicator startAnimating];
     
-    dispatch_async(dispatch_get_main_queue(), ^{
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_HIGH, 0), ^{
         [self displayProduct];
         [self createProduct];
     });
@@ -59,20 +59,22 @@
 {
     self.parser = [Parser parserWithProviderName:self.provider.name productURLString:self.productURLString];
     
-    NSNumber *priceInDollars = [(Price *)[self.parser.delegate productPrice] dollarAmount];
-    self.productNameLabel.text = [self.parser.delegate productName];
-    self.currentPriceLabel.text = [NSString stringWithFormat:@"$%.2f", [priceInDollars floatValue]];
-    self.priceSlider.maximumValue = [priceInDollars floatValue];
-    self.priceSlider.value = [priceInDollars floatValue] * 0.8;
-    self.wishPriceLabel.text = [NSString stringWithFormat:@"$%.2f", ([priceInDollars floatValue] * 0.8)];
-    [self.view viewWithTag:1].alpha = 0;
-    
-    Image *image = [self.parser.delegate productImage];
-    [image downloadImageFromURL:[NSURL URLWithString:image.externalURLString] completionBlock:^(BOOL succeeded, UIImage *image) {
-        self.imageView.image = image;
-        self.imageView.contentMode = UIViewContentModeScaleAspectFit;
-        [self.activityIndicator stopAnimating];
-    }];
+    dispatch_sync(dispatch_get_main_queue(), ^{
+        NSNumber *priceInDollars = [(Price *)[self.parser.delegate productPrice] dollarAmount];
+        self.productNameLabel.text = [self.parser.delegate productName];
+        self.currentPriceLabel.text = [NSString stringWithFormat:@"$%.2f", [priceInDollars floatValue]];
+        self.priceSlider.maximumValue = [priceInDollars floatValue];
+        self.priceSlider.value = [priceInDollars floatValue] * 0.8;
+        self.wishPriceLabel.text = [NSString stringWithFormat:@"$%.2f", ([priceInDollars floatValue] * 0.8)];
+        [self.view viewWithTag:1].alpha = 0;
+        
+        Image *image = [self.parser.delegate productImage];
+        [image downloadImageFromURL:[NSURL URLWithString:image.externalURLString] completionBlock:^(BOOL succeeded, UIImage *image) {
+            self.imageView.image = image;
+            self.imageView.contentMode = UIViewContentModeScaleAspectFit;
+            [self.activityIndicator stopAnimating];
+        }];
+    });
 }
 
 - (void)createProduct

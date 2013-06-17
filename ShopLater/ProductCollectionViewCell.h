237f01ec5b3bc07ba81a9007ce8b83aa8de1a426
Natasha Murashev <nmurashev@gmail.com>
2013-06-17 15:01:29 -0500

    made shopping list into collection view

---
+++ ShopLater/ProductCollectionViewCell.h
@@ -0,0 +1,19 @@
+//
+//  ProductCollectionViewCell.h
+//  ShopLater
+//
+//  Created by Natasha Murashev on 6/17/13.
+//  Copyright (c) 2013 Natasha Murashev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "ProductDelegate.h"
+#import "Product+SLExtensions.h"
+
+@interface ProductCollectionViewCell : UICollectionViewCell
+
+@property (strong, nonatomic) Product *product;
+
+@property (strong, nonatomic) id<ProductDelegate> delegate;
+
+@end
---
+++ ShopLater/ShoppingListViewController.h
@@ -0,0 +1,14 @@
+//
+//  ShoppingListViewController.h
+//  ShopLater
+//
+//  Created by Natasha Murashev on 6/17/13.
+//  Copyright (c) 2013 Natasha Murashev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "ProductDelegate.h"
+
+@interface ShoppingListViewController : UICollectionViewController <ProductDelegate>
+
+@end

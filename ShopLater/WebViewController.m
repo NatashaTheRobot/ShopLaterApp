ae6af580a2b5c8891af88e13d1799768d85aabb1
Natasha Murashev <nmurashev@gmail.com>
2013-06-18 15:39:52 -0500

    removed nslog in webviewcontroller

--- ShopLater/WebViewController.m
+++ ShopLater/WebViewController.m
@@ -128,10 +128,6 @@
 
 - (void)revealMenu
 {
-    [self.view.subviews enumerateObjectsUsingBlock:^(UIView *view, NSUInteger idx, BOOL *stop) {
-        NSLog(@"%@", [view class]);
-    }];
-    
     [self.webView stringByEvaluatingJavaScriptFromString:@"document.activeElement.blur()"];
     ((MenuViewController *)self.slidingViewController.underLeftViewController).selectedProvider = self.provider;
     [self.slidingViewController anchorTopViewTo:ECRight];
